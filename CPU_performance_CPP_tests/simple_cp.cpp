e1d0a771d7fef97052bca1700eb044079476f985
WenqiJiang <wenqi.j@columbia.edu>
2022-01-27 13:36:50 +0100

    update

--- CPU_performance_CPP_tests/simple_cp.cpp
+++ CPU_performance_CPP_tests/simple_cp.cpp
@@ -33,6 +33,8 @@ int main() {
     auto t0 = std::chrono::high_resolution_clock::now();
     double duration;
 
+    uint64_t tmp_array[CODE_SIZE];
+
     for (int outer_i = 0; outer_i < 3; outer_i++) {
 
         std::cout << "\n=== Iteration " << outer_i << "===" << std::endl;
@@ -50,6 +52,57 @@ int main() {
              std::chrono::high_resolution_clock::now() - t0).count() / 1000.0;
         print_throughput(duration, total_size);
 
+        // cp from src to destimation (manual unroll 16 times)
+        std::cout << "\ncp from src to destimation (manual unroll 16 times): \n";
+        t0 = std::chrono::high_resolution_clock::now();
+        for (int j = 0; j < num_vectors; j++) {
+            tmp_array[0] = codes_cp_from[0];
+            tmp_array[1] = codes_cp_from[1];
+            tmp_array[2] = codes_cp_from[2];
+            tmp_array[3] = codes_cp_from[3];
+            tmp_array[4] = codes_cp_from[4];
+            tmp_array[5] = codes_cp_from[5];
+            tmp_array[6] = codes_cp_from[6];
+            tmp_array[7] = codes_cp_from[7];
+            tmp_array[8] = codes_cp_from[8];
+            tmp_array[9] = codes_cp_from[9];
+            tmp_array[10] = codes_cp_from[10];
+            tmp_array[11] = codes_cp_from[11];
+            tmp_array[12] = codes_cp_from[12];
+            tmp_array[13] = codes_cp_from[13];
+            tmp_array[14] = codes_cp_from[14];
+            tmp_array[15] = codes_cp_from[15];
+        }
+        duration = std::chrono::duration_cast<milli>(
+             std::chrono::high_resolution_clock::now() - t0).count() / 1000.0;
+        print_throughput(duration, total_size);
+
+        // cp from src to destimation + uint64_t conversion (manual unroll 16 times)
+        std::cout << "\ncp from src to destimation + uint64_t conversion (manual unroll 16 times): \n";
+        t0 = std::chrono::high_resolution_clock::now();
+        for (int j = 0; j < num_vectors; j++) {
+            tmp_array[0] = (uint64_t) codes_cp_from[0];
+            tmp_array[1] = (uint64_t) codes_cp_from[1];
+            tmp_array[2] = (uint64_t) codes_cp_from[2];
+            tmp_array[3] = (uint64_t) codes_cp_from[3];
+            tmp_array[4] = (uint64_t) codes_cp_from[4];
+            tmp_array[5] = (uint64_t) codes_cp_from[5];
+            tmp_array[6] = (uint64_t) codes_cp_from[6];
+            tmp_array[7] = (uint64_t) codes_cp_from[7];
+            tmp_array[8] = (uint64_t) codes_cp_from[8];
+            tmp_array[9] = (uint64_t) codes_cp_from[9];
+            tmp_array[10] = (uint64_t) codes_cp_from[10];
+            tmp_array[11] = (uint64_t) codes_cp_from[11];
+            tmp_array[12] = (uint64_t) codes_cp_from[12];
+            tmp_array[13] = (uint64_t) codes_cp_from[13];
+            tmp_array[14] = (uint64_t) codes_cp_from[14];
+            tmp_array[15] = (uint64_t) codes_cp_from[15];
+        }
+        duration = std::chrono::duration_cast<milli>(
+             std::chrono::high_resolution_clock::now() - t0).count() / 1000.0;
+        print_throughput(duration, total_size);
+
+
         // in cache copy performance: only copy the first bytes
         std::cout << "\nin cache copy performance: only copy the first bytes: \n";
         t0 = std::chrono::high_resolution_clock::now();
@@ -65,7 +118,6 @@ int main() {
 
         // in cache copy (with uint8_t -> uint64_t transform)
         std::cout << "\nin cache copy (with uint8_t -> uint64_t transform): \n";
-        uint64_t tmp_array[CODE_SIZE];
         t0 = std::chrono::high_resolution_clock::now();
         for (int j = 0; j < num_vectors; j++) {
 #pragma unroll(16)
@@ -79,7 +131,6 @@ int main() {
 
         // in cache copy manual unroll (with uint8_t -> uint64_t transform)
         std::cout << "\nin cache copy manual unroll (with uint8_t -> uint64_t transform): \n";
-        // uint64_t tmp_array[CODE_SIZE];
         t0 = std::chrono::high_resolution_clock::now();
         for (int j = 0; j < num_vectors; j++) {
             tmp_array[0] = (uint64_t) codes_cp_from[0];
